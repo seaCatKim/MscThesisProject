250fcc5308980caf9ced0df17d0ae359bd1cc076
Adriano Fossati <adriano.fossati@netplus.ch>
2024-02-16 15:34:11 +1000

    Commit infos on how to comment/get comments

--- ScriptMscThesis.R
+++ ScriptMscThesis.R
@@ -1,2 +1,5 @@
 #Script for my Msc thesis project: 
 
+### SO here as I will add some code, everyone will be able to see it. 
+
+###@Catherine, how does it work tho with you commenting my code etc...? 
